90fb6358aa77495e09f8575a5a0b46b84aa5c7e7
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2025-05-29 20:28:05 -0300

    feat: adding home, functionality to list musicians, artists and albums

--- projeto-pw-angular/src/app/app.routes.ts
+++ projeto-pw-angular/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "**", component: HomeComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- projeto-pw-angular/src/app/components/home/home.component.ts
+++ projeto-pw-angular/src/app/components/home/home.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-}
+  musicasData = [
+    { name: 'ClickBait', image: 'assets/musicas-dosPredios.jpg' },
+    { name: 'Não temos Medo', image: 'assets/musicas-naoTemosMedo.jpeg' },
+    { name: 'Mãe Solteira', image: 'assets/musicas-maeSolteira.jpg' },
+    { name: 'Deixa eu ver se ta Lisinha', image: 'assets/musicas-deixaEuVer.jpeg' },
+    { name: 'Taylor', image: 'assets/musicas-taylor.jpg' },
+    { name: 'Three Little Birds', image: 'assets/musicas-bobMarley.jpg' }
+  ];
+
+  artistData = [
+    { name: 'Henrique & Juliano', image: 'assets/artista-henrique-juliano.jpg' },
+    { name: 'Jorge & Mateus', image: 'assets/artista-jorge-mateus.jpg' },
+    { name: 'Zé Neto & Cristiano', image: 'assets/artista-ze-neto.jpg' },
+    { name: 'Gusttavo Lima', image: 'assets/artista-gusttavo-lima.jpg' },
+    { name: 'Luan Santana', image: 'assets/artista-luan-santana.jpg' },
+    { name: 'Matheus & Kauan', image: 'assets/artista-mateus-kauan.jpg' }
+  ];
+
+  albumsData = [
+    { name: 'White Noise (sleep & Relaxation Sounds)', artist: 'Sleepy John', image: 'assets/album-white-noise.jpg' },
+    { name: 'O Céu Explica Tudo(Ao Vivo)', artist: 'Henrique & juliano', image: 'assets/album-ceu-explica.jpg' },
+    { name: 'Nada como um dia após o outro', artist: 'Racionais', image: 'assets/album-vida-loka.jpg' },
+    { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: 'assets/album-hit-me.jpg' },
+    { name: 'CAJU', artist: 'Liniker', image: 'assets/album-caju.jpg' },
+    { name: 'Escândalo Íntimo', artist: 'Luisa Sonza', image: 'assets/album-escandalo.jpg' }
+  ];
+
+
+
+
+};
