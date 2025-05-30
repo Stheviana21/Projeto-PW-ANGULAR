a13cd6ae2a90c4ce8b1d5e45e2ede57f63d22907
Sthephany Viana da Silva <sthephanyv021@gmail.com>
2025-05-29 21:37:48 -0300

    feat: implement login component with form validation and styling

--- projeto-pw-angular/src/app/components/login/login.component.ts
+++ projeto-pw-angular/src/app/components/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  showPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+      // Add your authentication logic here
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
